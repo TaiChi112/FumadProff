96575be3880431eb0f6078830e24501a927fcdcd
TaiChi <anothai.0978452316@gmail.com>
2025-10-05 22:31:10 +0700

    feat: add blog page and layout components for dynamic blog routing

---
+++ app/blogs/layout.tsx
@@ -0,0 +1,11 @@
+import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { baseOptions } from '@/lib/layout.shared';
+import { blogsSource } from '@/lib/source';
+
+export default function Layout({ children }: LayoutProps<'/blogs'>) {
+  return (
+    <DocsLayout tree={blogsSource.pageTree} {...baseOptions()}>
+      {children}
+    </DocsLayout>
+  );
+}
